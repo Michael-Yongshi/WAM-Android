ef430feec38f576de182c7bc8bb6ee941211d826
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2020-05-22 16:54:44 +0200

    default from android studio empty activit

---
+++ app/src/main/java/com/yongshi42/wam_android/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.yongshi42.wam_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
