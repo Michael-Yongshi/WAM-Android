f02eb76d99408d874c9577eb67aba92e7d74ff98
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2020-05-26 16:34:27 +0200

    nfc imports

--- app/src/main/java/com/yongshi42/wam_android/MainActivity.kt
+++ app/src/main/java/com/yongshi42/wam_android/MainActivity.kt
@@ -1,7 +1,21 @@
 package com.yongshi42.wam_android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.PendingIntent
+import android.content.Intent
+
+import android.nfc.NdefMessage
+import android.nfc.NdefRecord
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.MifareClassic
+import android.nfc.tech.MifareUltralight
+
 import android.os.Bundle
+import android.os.Parcelable
+import android.widget.TextView
+import android.widget.Toast
+
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
